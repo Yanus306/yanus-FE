261c9b3a826b165ace5c48e47c28524499ddfca4
Radian06 <aksen05aksen@gmail.com>
2026-03-25 20:59:34 +0900

    chore: 소개 페이지 폴더 및 파일 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Intro from './pages/Intro.jsx'
+import Intro from './pages/intro/Intro.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
