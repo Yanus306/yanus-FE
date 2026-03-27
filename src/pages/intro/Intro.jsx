245008eff9cb3b147219ed5903674bb6e13524ad
Radian06 <aksen05aksen@gmail.com>
2026-03-27 18:17:27 +0900

    chore: intro visual section 관련 파일 생성

--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -1,9 +1,11 @@
 import Header from '../../widgets/layout/ui/header.jsx';
+import VisualSection from '../../widgets/intro/visualSection.jsx';
 
 function Intro() {
   return (
     <>
       <Header />
+      <VisualSection />
     </>
   )
 }
---
+++ src/widgets/intro/visualSection.jsx
@@ -0,0 +1,9 @@
+import "../intro/visualSection.css";
+
+const VisualSection = () => (
+    <div className="visual-section">
+        test
+    </div>
+);
+
+export default VisualSection
