35e2cf73b6ebfd558130bf396d70337a9fce9b12
Radian06 <aksen05aksen@gmail.com>
2026-03-28 15:34:23 +0900

    chore: intro history section 관련 파일 생성

---
+++ src/widgets/intro/historySection/ui/historySection.jsx
@@ -0,0 +1,11 @@
+import "./historySection.css";
+
+const HistorySection = () => {
+  return (
+    <div className="history-section">
+        temp
+    </div>
+  );
+};
+
+export default HistorySection;
