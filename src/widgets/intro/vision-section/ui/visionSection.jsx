c712e6fdf172c49a62fff976174f76cc35169d96
Radian06 <aksen05aksen@gmail.com>
2026-03-28 11:57:27 +0900

    chore: intro vision section 관련 파일 생성

---
+++ src/widgets/intro/vision-section/ui/visionSection.jsx
@@ -0,0 +1,11 @@
+import "./visionSection.css";
+
+const VisionSection = () => {
+  return (
+    <div className="vision-section">
+        temp
+    </div>
+  );
+};
+
+export default VisionSection;
