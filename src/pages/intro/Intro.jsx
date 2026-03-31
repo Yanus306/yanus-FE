ebf79cb89d83609269697361ea9e1d9a89c3fd60
Radian06 <aksen05aksen@gmail.com>
2026-03-31 21:57:23 +0900

    feat: footer 구현

--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -3,6 +3,7 @@ import VisualSection from '../../widgets/intro/visual-section/ui/visualSection.j
 import VisionSection from '../../widgets/intro/vision-section/ui/visionSection.jsx';
 import HistorySection from '../../widgets/intro/historySection/ui/historySection.jsx';
 import ProfessorSection from '../../widgets/intro/professorSection/ui/professorSection.jsx';
+import Footer from '../../widgets/layout/ui/footer/footer.jsx';
 
 function Intro() {
   return (
@@ -12,6 +13,7 @@ function Intro() {
       <VisionSection />
       <HistorySection />
       <ProfessorSection />
+      <Footer />
     </>
   )
 }
