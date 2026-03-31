55efb0e0173f13ec3986487d2b1243d328040f00
Radian06 <aksen05aksen@gmail.com>
2026-03-31 21:39:57 +0900

    chore: footer 관련 파일 생성 및 구조 변경

--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -1,4 +1,4 @@
-import Header from '../../widgets/layout/ui/header.jsx';
+import Header from '../../widgets/layout/ui/header/header.jsx';
 import VisualSection from '../../widgets/intro/visual-section/ui/visualSection.jsx';
 import VisionSection from '../../widgets/intro/vision-section/ui/visionSection.jsx';
 import HistorySection from '../../widgets/intro/historySection/ui/historySection.jsx';
