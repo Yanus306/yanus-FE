1da688dbdbbeb4891ba5878a560fee4c58d08b6f
Radian06 <aksen05aksen@gmail.com>
2026-04-19 22:13:52 +0900

    feat: intro member 첫 페이지 UI 구현

--- src/widgets/intro/member-section/ui/memberSection.jsx
+++ src/widgets/intro/member-section/ui/memberSection.jsx
@@ -1,11 +1,26 @@
 import "./memberSection.css";
+import Divider from "../../../../shared/ui/divider.jsx";
+import YanusGif from "/src/assets/yanus.gif";
 
 const MemberSection = () => {
-  return (
-    <div className="member-section">
-        
-    </div>
-  );
+
+    return (
+        <div className="member-section">
+            <Divider />
+
+            <div className="member-title">멤버 소개</div>
+
+            <div className="member-box">
+                <div className="member-tabs">
+                    <img src={YanusGif} alt="" className="member-gif"></img>
+                </div>
+                
+                <div className="member-mark-box">
+                    <div className="member-bookmark">1기</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default MemberSection;
