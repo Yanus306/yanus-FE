318c26644e532c25cc0117c82e023d7dbadf7751
Radian06 <aksen05aksen@gmail.com>
2026-04-27 16:58:59 +0900

    feat: intro member 셀장 아이콘 추가

--- src/widgets/intro/member-section/ui/memberSection.jsx
+++ src/widgets/intro/member-section/ui/memberSection.jsx
@@ -1,62 +1,70 @@
 import "./memberSection.css";
 import Divider from "../../../../shared/ui/divider.jsx";
 import YanusGif from "/src/assets/yanus.gif";
+import CrownIcon from "/src/assets/crownIcon.png";
 import { useState } from "react";
 import { MEMBER_DATA } from "/src/shared/datas/memberData.js";
 
 const MemberSection = () => {
-    const [activeTab, setActiveTab] = useState(null);
+  const [activeTab, setActiveTab] = useState(null);
 
-    return (
-        <div className="member-section">
-            <Divider />
+  return (
+    <div className="member-section">
+      <Divider />
 
-            <div className="member-title">멤버 소개</div>
+      <div className="member-title">멤버 소개</div>
 
-            <div className="member-box">
-                <div className={`member-tabs ${activeTab ? "active" : ""}`}>
-                    {activeTab ? (
-                        <table className="member-table">
-                            <thead>
-                                <tr>
-                                    <th>기수</th>
-                                    <th>이름</th>
-                                    <th></th>
-                                    <th>분야</th>
-                                    <th>비고?</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {MEMBER_DATA[activeTab].map((member, index) => (
-                                    <tr key={index}>
-                                        <td>{member.gen}</td>
-                                        <td>{member.name}</td>
-                                        <td>{member.role}</td>
-                                        <td>{member.field}</td>
-                                        <td>{member.note}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    ) : (
-                        <img src={YanusGif} alt="Intro" className="member-gif" />
-                    )}
-                </div>
-                
-                <div className="member-mark-box">
-                    {Object.keys(MEMBER_DATA).map((gen) => (
-                        <div 
-                            key={gen} 
-                            className={`member-bookmark ${activeTab === gen ? "active" : ""}`}
-                            onClick={() => setActiveTab(gen)}
-                        >
-                            {gen}
-                        </div>
-                    ))}
-                </div>
-            </div>
+      <div className="member-box">
+        <div className={`member-tabs ${activeTab ? "active" : ""}`}>
+          {activeTab ? (
+            <table className="member-table">
+              <thead>
+                <tr>
+                  <th>기수</th>
+                  <th>이름</th>
+                  <th></th>
+                  <th>분야</th>
+                  <th>비고?</th>
+                </tr>
+              </thead>
+              <tbody>
+                {MEMBER_DATA[activeTab].map((member, index) => (
+                  <tr key={index}>
+                    <td>{member.gen}</td>
+                    <td>{member.name}</td>
+                    <td>
+                      <img
+                        src={CrownIcon}
+                        alt="crown"
+                        className="member-crownIcon"
+                      />
+                        <span className="member-role">{member.role}</span>
+                    </td>
+                    <td>{member.field}</td>
+                    <td>{member.note}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <img src={YanusGif} alt="Intro" className="member-gif" />
+          )}
         </div>
-    );
+
+        <div className="member-mark-box">
+          {Object.keys(MEMBER_DATA).map((gen) => (
+            <div
+              key={gen}
+              className={`member-bookmark ${activeTab === gen ? "active" : ""}`}
+              onClick={() => setActiveTab(gen)}
+            >
+              {gen}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MemberSection;
