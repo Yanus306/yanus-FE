b209c8e4544f305eb3d637664112300116cd9bd3
Radian06 <aksen05aksen@gmail.com>
2026-05-20 00:08:45 +0900

    feat(#19): intro professor 한 마디 추가

--- src/widgets/intro/professorSection/ui/professorSection.jsx
+++ src/widgets/intro/professorSection/ui/professorSection.jsx
@@ -11,23 +11,30 @@ const ProfessorSection = () => {
             
             {/* 상단 프로필 영역 */}
             <div className="professor-upper-container">
-                <div className="professor-img-wrapper">
-                    <img src={ProfessorImg} alt="" className="professor-img"></img>
+                <div className="professor-upper-box">
+                    <div className="professor-img-wrapper">
+                        <img src={ProfessorImg} alt="" className="professor-img"></img>
+                    </div>
+                    
+                    <div className="professor-profileBox">
+                        <div className="professor-info-header">
+                            <span className="professor-name">{professorInfo.name}</span>
+                            <span className="professor-position">{professorInfo.position}</span>
+                        </div>
+                        <div className="professor-info-list">
+                            {professorInfo.details.map((item) => (
+                                <div className="professor-info-item" key={item.label}>
+                                    <span className="info-label">{item.label}</span>
+                                    <span className="info-value">{item.value}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-                
-                <div className="professor-profileBox">
-                    <div className="professor-info-header">
-                        <span className="professor-name">{professorInfo.name}</span>
-                        <span className="professor-position">{professorInfo.position}</span>
-                    </div>
-                    <div className="professor-info-list">
-                        {professorInfo.details.map((item) => (
-                            <div className="professor-info-item" key={item.label}>
-                                <span className="info-label">{item.label}</span>
-                                <span className="info-value">{item.value}</span>
-                            </div>
-                        ))}
-                    </div>
+
+                <div className="professor-lower-box">
+                    <span className="professor-quote">"국립경국대학교 소프트웨어융합학과에서 정보보호를 공부하는 학생들이 대박나기를, </span>
+                    <span className="professor-quote">경북을 넘어 세계에서 우뚝 서기를 응원합니다."</span>
                 </div>
             </div>
 
