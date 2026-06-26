020421216775f9e18ff1d5c6956c8e19c6b70111
Radian06 <aksen05aksen@gmail.com>
2026-06-26 22:29:42 +0900

    feat(#23): auth Widget(전체 UI) 구현

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+
 import "../app/styles/index.css";
 import Intro from '../pages/intro/Intro.jsx';
+import Login from '../pages/auth/Login.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Intro />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} /> 
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,12 @@
+import { AuthWidget } from '../../widgets/auth/authWidget';
+// import { AuthForm } from '../../features/auth/authForm';
+
+const Login = () => {
+  return (
+    <AuthWidget title="로그인" bottomText="계정이 없으신가요?" bottomLink="회원가입">
+    
+    </AuthWidget>
+  );
+};
+
+export default Login;
